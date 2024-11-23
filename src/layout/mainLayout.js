39b27d07d4c8f5cbb96411101ddfa89a37c8b45b
trungnguyen29775 <ITITIU20332@student.hcmiu.edu.vn>
2024-11-23 16:53:55 +0700

    11/23/2024: Side bar

--- src/layout/mainLayout.js
+++ src/layout/mainLayout.js
@@ -3,20 +3,23 @@ import TinderCards from '../components/swiper/swiper';
 import { useContext, useEffect } from 'react';
 import StateContext from '../context/context.context';
 import { useNavigate } from 'react-router-dom';
+import Sidebar from '../components/sidebar/sidebar';
 
 const MainLayout = () => {
     const [state, dispatchState] = useContext(StateContext);
     const navigate = useNavigate();
-    useEffect(() => {
-        if (state.login === false) {
-            navigate('/login');
-        }
-    }, [state]);
+    // useEffect(() => {
+    //     if (state.login === false) {
+    //         navigate('/login');
+    //     }
+    // }, [state]);
 
     return (
         <Box sx={{ height: '100vh', width: '100%', display: 'flex', flexDirection: 'row' }}>
-            <Box sx={{ height: '100%', width: '20%' }}></Box>
-            <Box sx={{ height: '100%', width: '80%' }}>
+            <Box sx={{ height: '100%', minWidth: '250px' }}>
+                <Sidebar />
+            </Box>
+            <Box sx={{ height: '100%', minWidth: '60%', maxWidth: '80%' }}>
                 <TinderCards />
             </Box>
         </Box>
